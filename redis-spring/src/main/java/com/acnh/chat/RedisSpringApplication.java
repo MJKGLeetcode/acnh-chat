27d600be41e533095bbdeecb626c7b7500e30af0
MJKGLeetcode <keshavgupta9@outlook.com>
2024-05-15 19:51:32 -0400

    acnh-chat-initial-commit

---
+++ redis-spring/src/main/java/com/acnh/chat/RedisSpringApplication.java
@@ -0,0 +1,15 @@
+package com.acnh.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@EnableCaching
+@SpringBootApplication
+public class RedisSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisSpringApplication.class, args);
+	}
+
+}
---
+++ redis-spring/src/main/java/com/acnh/chat/service/ChatRoomService.java
@@ -0,0 +1,60 @@
+package com.acnh.chat.service;
+
+import org.redisson.api.RListReactive;
+import org.redisson.api.RTopicReactive;
+import org.redisson.api.RedissonReactiveClient;
+import org.redisson.client.codec.StringCodec;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.WebSocketMessage;
+import org.springframework.web.reactive.socket.WebSocketSession;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.Disposable;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+
+@Service
+public class ChatRoomService implements WebSocketHandler {
+
+    @Autowired
+    private RedissonReactiveClient client;
+
+    @Override
+    public Mono<Void> handle(WebSocketSession websocketSession) {
+
+
+        String room = getChatRoomName(websocketSession);
+        RTopicReactive topic = this.client.getTopic(room, StringCodec.INSTANCE);
+
+        RListReactive<String> list = this.client.getList("history:" + room, StringCodec.INSTANCE); // to get history of text for new member
+        // subscribe
+        websocketSession.receive()
+                .map(WebSocketMessage::getPayloadAsText)
+                .flatMap(msg -> list.add(msg).then(topic.publish(msg)))
+                .doOnError(System.out::println)
+                .doFinally(s -> System.out.println("Subscribe Finally " + s))
+                .subscribe();
+
+        // we are publisher here
+        Flux<WebSocketMessage> flux = topic.getMessages(String.class)
+                .startWith(list.iterator())
+                .map(websocketSession::textMessage)
+                .doOnError(System.out::println)
+                .doFinally(s -> System.out.println("Publisher Finally " + s));
+
+        return websocketSession.send(flux);
+    }
+
+    private String getChatRoomName(WebSocketSession socketSession) {
+
+        URI uri = socketSession.getHandshakeInfo().getUri();
+        return UriComponentsBuilder.fromUri(uri)
+                .build()
+                .getQueryParams()
+                .toSingleValueMap()
+                .getOrDefault("room", "default");
+    }
+}
